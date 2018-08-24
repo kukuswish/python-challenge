dd55eae0c3e0c44ca1d4ce361ebd608600e805f3
STirso <kukuswish@gmail.com>
2018-08-24 01:00:56 -0400

    Added average change.

--- PyBank/main.py
+++ PyBank/main.py
@@ -24,27 +24,23 @@ with open(csvpath, newline='') as csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
         monthCount += 1
         total = total + float(row[1])
 
-        
-
         if(monthCount != 0):
             currentMonth= float(row[1])
             change = getIncrease(currentMonth,prevMonth)
 
-            currentIncrease += change
-            print(f"{row[0]} {change} {prevMonth} {currentMonth} {currentIncrease}")
+            currentIncrease = currentIncrease + change
             prevMonth=currentMonth   
         else:
-            currentIncrease+= float(row[1])
+            # currentIncrease+= float(row[1])
             prevMonth=float(row[1])
 
-        
+        #check for greatest increase and greatest decrease in profits
         if(change >= 0 and change > gIncrease):
             gIncrease= change
             gMonth = row[0]
@@ -58,6 +54,6 @@ print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {monthCount}")
 print(f"Total: ${int(total)}")
-print(f"Average  Change: $-2315.12 {averIncrease}")
+print(f"Average  Change: ${round(averIncrease,2)}")
 print(f"Greatest Increase in Profits: {gMonth} ({int(gIncrease)})")
 print(f"Greatest Decrease in Profits: {iMonth} ({int(gDec)})")
